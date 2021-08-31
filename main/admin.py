58998f4eb6e4aaa3e4a4ec37078d334f69e6aa17
osamamhd <osamamhd82@gmail.com>
2021-08-31 16:33:24 +0200

    [main] boatowner, boat, laod, agent, order and orderload models added.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import BoatOwner, Boat, Load, Agent, Order, OrderLoad
 
-# Register your models here.
+@admin.register(Boat)
+class BoatAdmin(admin.ModelAdmin):
+    list_display = ['name', 'quantity', 'fish_type', 'boat_owner']
+@admin.register(BoatOwner)
+class BoatOwnerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Load)
+class LoadAdmin(admin.ModelAdmin):
+    list_display = ['boat', 'delivery_date', 'batch_no', ]
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'join_date',
+        'agent_id',
+    ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'agent',
+        'ordered_at',
+    ]
+
+
+@admin.register(OrderLoad)
+class OrderLoadAdmin(admin.ModelAdmin):
+    list_display = [
+        'order',
+        'quantity',
+        'fish_type',
+        'pending',
+    ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,111 @@
 from django.db import models
+from dateutil.relativedelta import *
+from django.db.models import Sum
+from autoslug import AutoSlugField
 
-# Create your models here.
+
+FISH_TYPE = [
+    ('tilapia', 'Tilapia'),
+    ('mackerel', 'Mackerel'),
+    ('grouper', 'Grouper')
+]
+
+
+class BoatOwner(models.Model):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from='name')
+
+    def tilapia_sum(self):
+        sum = BoatOwner.objects.filter(pk=self.pk, boats__fish_type='tilapia').aggregate(Sum('boats__quantity'))
+        return sum['boats__quantity__sum']
+
+    def mackerel_sum(self):
+        sum = BoatOwner.objects.filter(pk=self.pk, boats__fish_type='mackerel').aggregate(Sum('boats__quantity'))
+        return sum['boats__quantity__sum']
+
+    def grouper_sum(self):
+        sum = BoatOwner.objects.filter(pk=self.pk, boats__fish_type='grouper').aggregate(Sum('boats__quantity'))
+        return sum['boats__quantity__sum']
+
+    def get_absolute_url(self):
+        return f'/{self.pk}/{self.slug}/'
+
+
+class Boat(models.Model):
+    boat_owner = models.ForeignKey(BoatOwner, related_name='boats', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantity = models.DecimalField(max_digits=8, decimal_places=2)
+    fish_type = models.CharField(max_length=50, choices=FISH_TYPE)
+
+    def __str__(self):
+        return self.name
+
+
+class Load(models.Model):
+    boat = models.ForeignKey(Boat, on_delete=models.CASCADE)
+    delivery_date = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return f'/{self.pk}/'
+
+    def format_delivery_date(self):
+        return self.delivery_date.strftime('%b,%d %Y @ %H:%M')
+
+    def batch_delivery_date(self):
+        return self.delivery_date.strftime('%m-%d-%Y-at-%H-%M')
+
+    def batch_no(self):
+        return f'{self.boat.name}-{self.batch_delivery_date()}-{self.boat.fish_type}'
+
+    def expiration_date(self):
+        date = self.delivery_date + relativedelta(months=+8)
+        return date.strftime('%b,%d %Y @ %H:%M')
+
+
+class Agent(models.Model):
+    name = models.CharField(max_length=50)
+    load = models.ForeignKey(Load, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def join_date(self):
+        return self.date.strftime('%b,%d %Y @ %H:%M')
+
+    def agent_id(self):
+        return self.date.strftime('%m%d%y%H%M%S')
+
+
+class Order(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def ordered_at(self):
+        return self.date.strftime('%b,%d %Y @ %H:%M')
+
+    def order_id(self):
+        return self.date.strftime('%m%d-%Y%H-%M%S')
+
+
+class OrderLoad(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=8, decimal_places=2)
+    fish_type = models.CharField(max_length=50, choices=FISH_TYPE)
+    pending = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        tilapia_sum = Boat.objects.filter(fish_type='tilapia').aggregate(Sum('quantity'))
+        mackerel_sum = Boat.objects.filter(fish_type='mackerel').aggregate(Sum('quantity'))
+        grouper_sum = Boat.objects.filter(fish_type='grouper').aggregate(Sum('quantity'))
+
+        if self.quantity > tilapia_sum['quantity__sum'] and self.fish_type == 'tilapia':
+            self.pending = True
+
+        if self.quantity > mackerel_sum['quantity__sum'] and self.fish_type == 'mackerel':
+            self.pending = True
+
+        if self.quantity > grouper_sum['quantity__sum'] and self.fish_type == 'grouper':
+            self.pending = True
+
+        super(OrderLoad, self).save(*args, **kwargs)
